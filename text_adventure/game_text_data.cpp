e3b08c96c0c07e7bb61eb2d404a9d31c18c1a781
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-01-08 19:24:34 -0800

    Resolve missing comma in map

--- text_adventure/game_text_data.cpp
+++ text_adventure/game_text_data.cpp
@@ -7,5 +7,7 @@ const int prompt_speed = 15;
 
 const std::unordered_map<std::string, slowly_printing_string> story_text {
   {"setting", slowly_printing_string{ "You wake up in a dense, eerie forest.\nThe air is thick with mist, and you have no memory of how you got here.\nA faint voice calls your name, echoing through the trees.\nYou must navigate the forest and uncover its secrets while deciding whether to trust the voice.\n", story_info_speed }},
-  {"beginning", slowly_printing_string{ "You hear a voice whisper:\n \"Come to me, and you will be free.\"\n", prompt_speed}}
+  {"beginning", slowly_printing_string{ "You hear a voice whisper:\n \"Come to me, and you will be free.\"\n", prompt_speed}},
+  {"three_paths", slowly_printing_string{ "You see three paths ahead:\n\n", prompt_speed}},
+
 };
