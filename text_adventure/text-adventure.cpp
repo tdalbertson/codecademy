b4075677effd45679f6b14210389b16be3825317
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2025-01-12 16:00:15 -0800

    Add code for entering raw mode & test code for reading up arrow input
    Entering raw mode code disables canonical mode and echo
    Reading up arrow input reads a buffer that holds an escape sequence

--- text_adventure/text-adventure.cpp
+++ text_adventure/text-adventure.cpp
@@ -22,7 +22,16 @@ Story map:
 #include <chrono>
 #include <thread>
 #include <unordered_map>
+#include <termios.h>
+#include <unistd.h>
 #include "GameText.h"
+
+// Function prototypes
+void disableRawMode(); 
+void enableRawMode();
+
+
+struct termios orig_termios;
 
 // Slow print string idea from https://wandbox.org/permlink/OAbOpSLiAeJRRfbI
 std::ostream& operator<<(std::ostream& out, const slowly_printing_string& s) {
@@ -34,10 +43,39 @@ std::ostream& operator<<(std::ostream& out, const slowly_printing_string& s) {
 }
 
 int main() {
-  const std::string textSeparator = "––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\n";
+    const std::string textSeparator = "––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\n";
 
-  std::cout << story_text.at("setting");
-  std::cout << story_text.at("beginning");
-  std::cout << textSeparator;
-  return 0;
+    std::cout << story_text.at("setting");
+    std::cout << story_text.at("beginning");
+    std::cout << textSeparator;
+
+    enableRawMode();
+
+    char buffer[3]; // Buffer to hold up to 3 bytes (ESC, [, A)
+    while(read(STDIN_FILENO, &buffer[0], 1) == 1 && buffer[0] != 'q') {
+        if (buffer[0] == '\033') { // Check if beginning of an escape sequence 
+            if (read(STDIN_FILENO, &buffer[1], 2) == 2) { // Check if 2 remaining bytes
+                if (buffer[1] == '[' && buffer[2] == 'A') { // Check if next 2 bytes match up arrow escape sequence
+                    std::cout << "You pressed the up arrow\n";
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+// Function definitions
+void disableRawMode() { // Idea from https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+} 
+
+void enableRawMode() { // From above link
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON); // Turn off input echo in terminal
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
